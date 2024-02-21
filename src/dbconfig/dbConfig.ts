fd2017906227bf77c1ca31cffa9237cf4c9c4ce0
Vishal <vishaljaiswal224@gmail.com>
2024-02-22 04:32:43 +0530

    signup and login ui

---
+++ src/dbconfig/dbConfig.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+export async function connectToDatabase() {
+  try {
+    mongoose.connect(process.env.MONGO_URI!);
+    const connection = mongoose.connection;
+
+    connection.on("connected", () => {
+      console.log("MONGODB CONNECTED SUCCESSFULLY");
+    });
+
+    connection.on("error", (err) => {
+      console.log(
+        "MogoDB connection error. Please make sure Mongodb is running." + err
+      );
+      process.exit();
+    });
+  } catch (error) {
+    console.log("something went wrong");
+    console.log(error);
+  }
+}
