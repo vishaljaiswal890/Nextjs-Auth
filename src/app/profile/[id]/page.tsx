5778f5d791c92814f4253cdc798cf7748c0c10be
Vishal <vishaljaiswal224@gmail.com>
2024-02-24 22:44:16 +0530

    Added middleware and me route

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,7 +1,7 @@
 export default function UserProfile({ params }: any) {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1>Profile</h1>
+      <h2 className="m-4 p-2">Profile</h2>
       <hr />
       <p className="text-4xl">
         Profile page
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -3,9 +3,11 @@ import axios from "axios";
 import Link from "next/link";
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function ProfilePage() {
   const router = useRouter();
+  const [data, setData] = useState("Nothing");
   const logout = async () => {
     try {
       await axios.get("/api/users/logout");
@@ -17,11 +19,22 @@ export default function ProfilePage() {
     }
   };
 
+  const getUserDetails = async () => {
+    const res = await axios.get("/api/users/me");
+    setData(res.data.data._id);
+  };
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1>Profile</h1>
       <hr />
       <p>Profile page</p>
+      <h2 className="p-2 rounded-md bg-violet-500 text-white font-bold">
+        {data === "Nothing" ? (
+          "Nothing"
+        ) : (
+          <Link href={`/profile/${data}`}>{data}</Link>
+        )}
+      </h2>
       <hr />
       <button
         onClick={logout}
@@ -29,6 +42,13 @@ export default function ProfilePage() {
       font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
       >
         Logout
+      </button>
+      <button
+        onClick={getUserDetails}
+        className="bg-gray-500 mt-4 hover:bg-gray-700 text-white
+      font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+      >
+        GetUser Details
       </button>
     </div>
   );
